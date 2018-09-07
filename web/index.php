759455e2283d3110c429175d0220f49b618f5a3a
Óscar <oscarvegaherrera59@gmail.com>
2018-09-07 13:45:28 +0200

    Añadido de htaccess

--- web/index.php
+++ web/index.php
@@ -1,6 +1,4 @@
 <?php
-echo 'hola';
-die();
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
